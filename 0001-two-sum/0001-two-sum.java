05141c9eebf0d6884107a878fa69452e73c94241
Aniket Gupta <cseews2318@glbitm.ac.in>
2026-02-16 23:31:06 +0530

    Time: 47 ms (11.27%), Space: 44.9 MB (99.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,54 @@
+import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
+        
+        for(int i =0;i<nums.length-1;i++)
+        {
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[]{i, j};
+                    
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int t =sc.nextInt();
+        ArrayList<Integer> num= new ArrayList<>();
+        while(true)
+        {
+          n=sc.nextInt();
+          if(n==-1) break;
+          num.add(n);
+
+        }
+        
+        int[] no= new int[num.size()];
+        for(int i=0;i<num.size();i++)
+        {
+            no[i]=num.get(i);
+        }
+        
+        
+
+        
+
+        Solution obj= new Solution();
+
+       int[] result = obj.twoSum(no, t);
+if (result != null) {
+    System.out.println(Arrays.toString(result));
+} else {
+    System.out.println("No pair found");
+}
+
+
     }
 }
