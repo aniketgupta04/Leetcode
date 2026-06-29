5ffeb6986f19e27889b7216983c911615701c0cf
Aniket Gupta <aniketgupta0099@gmail.com>
2026-06-29 17:30:35 +0530

    Time: 4 ms (15.18%) | Memory: 47.5 MB (80.08%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        HashSet<Integer> r= new HashSet<>();
+        HashSet<Integer> c= new HashSet<>();
+
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+
+            if(matrix[i][j]==0){
+                r.add(i);
+                c.add(j);
+            }
+            }
+        }
+
+         for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+
+                if(r.contains(i) || c.contains(j)){
+
+                    matrix[i][j]=0;
+                }
+            }
+         }
+        
+    }
+}
